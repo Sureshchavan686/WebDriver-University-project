b304f390bddfdfb546be5e3e46bb5897ca054cee
Suresh <suresh.chavan686@gmail.com>
2021-10-05 13:40:36 +0530

    Added Dummy.java

---
+++ src/test/java/com/utility/Dummy.java
@@ -0,0 +1,11 @@
+package com.utility;
+
+public class Dummy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello dummy class");
+	}
+
+}
